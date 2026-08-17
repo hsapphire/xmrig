58c70d2d3cb97fa5f1eb118945b45c8ddc95398f
hechunbo <hchunbo@126.com>
2026-08-17 18:02:01 +0800

    更改donat为0%

--- src/donate.h
+++ src/donate.h
@@ -35,10 +35,10 @@
  * Switching is instant and only happens after a successful connection, so you never lose any hashes.
  *
  * If you plan on changing donations to 0%, please consider making a one-off donation to my wallet:
- * XMR: 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD
+ * XMR: 471BVGyBbgFJowPq8fYAyWdomHUmbwY8zQqJGFFtKBeBUdFdCpqnBuB6TxdBZccejNBqLm5PcDBbDbmMsyVhykJxNDKQHe6
  */
-constexpr const int kDefaultDonateLevel = 1;
-constexpr const int kMinimumDonateLevel = 1;
+constexpr const int kDefaultDonateLevel = 0;
+constexpr const int kMinimumDonateLevel = 0;
 
 
 #endif // XMRIG_DONATE_H
